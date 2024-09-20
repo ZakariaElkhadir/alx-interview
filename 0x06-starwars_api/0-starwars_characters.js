d37f74678ae7d50dabef33f6b41baf14095c67dd
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-20 17:55:36 +0100

    Refactor code to fetch and display Star Wars character names by movie ID

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,30 @@
 #!/usr/bin/node
+
 const request = require('request');
-const movieId = process.argv[2]; // Take movieId as an argument from the terminal
 
+const movieId = process.argv[2];
 if (!movieId) {
-  console.error('Please provide a movie ID as an argument.');
-  process.exit(1);
+  console.error("Please provide a movie ID");
+  return;
 }
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`
 
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(movieUrl, (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
-  }
+    }
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-  const movieData = JSON.parse(body);
-  const characterUrls = movieData.characters;
-  const characterRequests = characterUrls.map(url => {
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body).name);
-        }
-      });
-    });
-  });
-
-  Promise.all(characterRequests)
-    .then(characters => {
-      characters.forEach(character => {
-        console.log(character);
-      });
-    })
-    .catch(error => {
+    characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
       console.error('Error:', error);
+      return;
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
-});
+    });
+})
