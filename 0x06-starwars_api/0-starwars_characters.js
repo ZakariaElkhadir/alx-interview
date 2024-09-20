cece566abffbd6bd2dc260f3adb9934a1bdf8550
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-20 18:25:43 +0100

    Refactor code to handle missing movie ID argument and improve error handling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,21 +2,28 @@
 const request = require('request');
 const movieId = process.argv[2]; // Take movieId as an argument from the terminal
 
-if (!movieId) {
+if (!movieId) { // if no argument id entered will log error
   console.error('Please provide a movie ID as an argument.');
   process.exit(1);
 }
-
+// url of the api
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(movieUrl, (error, response, body) => {
-  if (error) {
+  if (error) { // log error if the request fail
     console.error('Error:', error);
     return;
   }
 
   const movieData = JSON.parse(body);
   const characterUrls = movieData.characters;
+  /**
+   * Creates an array of Promises that fetch character names from given URLs.
+   *
+   * @constant {Promise<string>[]} characterRequests - An array of Promises, each resolving to a character name.
+   * @param {string[]} characterUrls - An array of URLs to fetch character data from.
+   * @returns {Promise<string>[]} An array of Promises, each resolving to a character name.
+   */
   const characterRequests = characterUrls.map(url => {
     return new Promise((resolve, reject) => {
       request(url, (error, response, body) => {
