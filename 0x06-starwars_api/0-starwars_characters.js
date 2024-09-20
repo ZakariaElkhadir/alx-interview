70062f64ca87870a0ff11391d3baa0884d1658ab
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-20 17:24:02 +0100

    Refactor code to fetch and display Star Wars character names by movie ID

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
-const n = 20;
-const requests = [];
-
-for (let i = 1; i <= n; i++) {
-  const request = new Request(`https://swapi-api.alx-tools.com/api/people/${i}/`);
-  requests.push(fetch(request).then(response => response.json()));
+const movieId = process.argv[2]; // Take movieId as an argument from the terminal
+if (!movieId) {
+  console.error('Please provide a movie ID as an argument.');
+  process.exit(1);
 }
+const movieRequest = new Request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`);
 
-Promise.all(requests)
+fetch(movieRequest)
+  .then(response => response.json())
+  .then(movieData => {
+    const characterUrls = movieData.characters;
+    const characterRequests = characterUrls.map(url => fetch(url).then(response => response.json()));
+
+    return Promise.all(characterRequests);
+  })
   .then(characters => {
     characters.forEach(character => {
       console.log(character.name);
