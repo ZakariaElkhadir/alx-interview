9332c91372d62c79c66ef88d2812f09022189c6e
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-09-27 10:02:09 +0100

    Refactor code to improve 2D matrix rotation algorithm

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-import numpy as np
+"""
+Rotate 2D Matrix
+"""
+
 
 def rotate_2d_matrix(matrix):
-    """Rotate the matrix 90 degrees clockwise."""
-    rotated_matrix = np.rot90(matrix, k=-1)  # k=-1 rotates 90 degrees clockwise
-    return rotated_matrix
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
