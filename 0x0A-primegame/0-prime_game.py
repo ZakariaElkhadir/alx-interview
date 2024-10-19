fee88bc931e65d271de8aff255f47fcaa91e9900
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-10-19 18:31:58 +0100

    Refactor code to optimize prime number counting algorithm

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,42 @@
-#!/use/bin/python3
-"""Prime Game"""
+#!/usr/bin/python3
+"""0. Prime Game - Maria and Ben are playing a game"""
+
 
 def isWinner(x, nums):
-    """Prime Game"""
-    def is_prime(n):
-        """Check if a number is prime"""
-        if n <= 1:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    def count_primes(n):
-        """Count the number of prime numbers up to n"""
-        count = 0
-        for i in range(2, n + 1):
-            if is_prime(i):
-                count += 1
-        return count
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for num in nums:
-        if count_primes(num) % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
         return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
